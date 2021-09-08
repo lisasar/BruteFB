fbef1bc64f8f1f330bc31dd29802d2c2ef0f63c0
TTermuxID <86596844+TTermuxID@users.noreply.github.com>
2021-09-08 15:03:21 +0700

    Add files via upload

---
+++ moshi_moshi/sandikrek.py
@@ -0,0 +1,20 @@
+# Ganti? Silahkan Kalo Eror Mampus Lu
+pw_tambahan="""sayang|anjing|freefire|bismillah|kontol|bangsat|bajingan|indonesia
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	return [ngentod,ngentod+"123",ngentod+"123456"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		aku_sange_sama_ara=ngentot[1].split(" ")
+		if len(aku_sange_sama_ara[ngntd])!=ngntd:
+			asu=awok(aku_sange_sama_ara[ngntd])
+			if len(aku_sange_sama_ara[ngntd])<=5:
+				del asu[ngntd]
+				for x in pw_tambahan: asu.append(x)
+			else:
+				for x in pw_tambahan: asu.append(x)
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
+ 
